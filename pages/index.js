6a7d538c48029831b26b60cfba531531b6eb9158
alonecheer <alonecheer@hotmail.com>
2019-11-02 18:49:48 +0700

    add Hot product 3 block

--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,26 @@ import MainLayout from '../layouts/MainLayout';
 const HomePage = () => {
     return (
         <MainLayout>
-            <div className="main">  
-            <div className="box" >
-                   stock 
-                </div>
-            
+            <div className="main">
+                
             </div>
             <style JSX>{`
                 .main {
                     display: flex;
-                    //background:LightGray;
-                    hight:500px;
+                    background:red;
+                    //hight:100px;
                     //width: 70%;
-                    padding:  0  80px; 
-                    
+                    //padding:  0  80px; 
                     justify-content: center;
                 }
-                .in-main {
-                    width:70%;
-                    border-top: 1px solid black;
-                    text-align: flex-start; 
-                    align-self: center;
-                }
-                .box {
+                .box1 {
                     width: 70%;
-                    height: 500px;
+                    //height: 500px;
                     line-height: 50px; 
-                    
+                    background:red;
                     color: OrangeRed ;
                     text-align: flex-start; 
+                    padding-left: 30px;
                     border-top: 1px solid black;
                   }
             `}</style>
--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,89 @@
 import MainLayout from '../layouts/MainLayout';
-const ProductPage = () =>{
+
+const ProductPage = () => {
     return (
         <MainLayout>
-            <div>Product</div> 
+            <div className="container-product-1">
+                <div className="box-product1">
+                    <span className="orangeText">Hot</span> <span className="blackText" Product>Product</span>
+                </div>
+            </div>
+
+            <div className="container-product-2">
+                <div className="box-product2">2</div>
+                <div className="box-product3" >3</div>
+                <div className="box-product4" >4</div>
+            </div>
+            <style JSX>{`
+                    .container-product-1 {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        //background: #37BC9B;
+                        //height: 200px;
+                      }
+                      .container-product-2 {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        //background: #37BC9B;
+                        //height: 200px;
+                      }
+                      
+                      .box-product1 {
+                        width: 70%;
+                        line-height: 60px; 
+                        //background: #434A54;
+                        color: black;
+                        font-size: 20px;
+                        border-top: 1px solid black;
+                        text-indent: 20px;
+                        align-self: flex-start;
+                        
+                      }
+                      .box-product2 {
+                        width: 250px;
+                        height: 200px;
+                        line-height: 50px; 
+                        background: #434A54;
+                        color: #F5F7FA;
+                        border: 1px solid #CCD1D9;
+                        text-align: center; 
+                        align-content: flex-start;
+                        margin: 70px 100px;
+                      }
+                      .box-product3 {
+                        width: 250px;
+                        height: 200px;
+                        line-height: 50px; 
+                        background: #434A54;
+                        color: #F5F7FA;
+                        border: 1px solid #CCD1D9;
+                        text-align: center; 
+                        margin: 70px 100px;
+                      }
+                      .box-product4 {
+                        width: 250px;
+                        height: 200px;
+                        line-height: 50px; 
+                        background: #434A54;
+                        color: #F5F7FA;
+                        border: 1px solid #CCD1D9;
+                        text-align: center; 
+                        margin: 70px 100px;
+                      }
+                      .orangeText  {
+                        color:orange;
+                        font-weight:bold;
+                        }
+                      .blackText    {
+                        color:black;
+                        font-weight:bold;
+                        }
+                `}
+            </style>
         </MainLayout>
-       
+
     )
 }
 export default ProductPage;
