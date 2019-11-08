a56118f1597fcea0e9f71fe96f578b1a7848adb9
alonecheer <alonecheer@hotmail.com>
2019-11-08 18:59:13 +0700

    commit

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -9,6 +9,7 @@ const MainLayout = props => {
                     <Link href='/'>
                         <a className='link' >STOCK</a>
                     </Link>
+                </div>
 
                 <div className='menu-container'>
                     <div>
@@ -26,28 +27,6 @@ const MainLayout = props => {
                             <a className='link'>Profile</a>
                         </Link>
                     </div>
-                </div>
-
-            </div>
-
-            <div className='team'>
-
-            </div>
-
-            <div className='search'>
-                {/* <i className="icon-search"></i> */}
-                <input className='text-search' type="text" placeholder="Search.." >
-                </input>
-                <button className="box1" type="submit">Search</button>
-            </div>
-            <div className="introduct">
-                <div className="boxL-introduct">
-                    <h1>Storage</h1>
-                    <h3>Keep your product data <br></br> into the database</h3>
-                </div>
-                <div className="boxR-introduct">
-                    <h1>Accessible</h1>
-                    <h3>The product Information <br></br> can be easily <br></br> accessed by anynoe</h3>
                 </div>
             </div>
 
@@ -69,7 +48,6 @@ const MainLayout = props => {
             <style jsx>{`
                .topbar {
                    display: flex;
-                // flex-direction: row;
                    justify-content: space-between;
                    align-items: center;
                    height: 32px;
@@ -93,102 +71,14 @@ const MainLayout = props => {
                 background-repeat: no-repeat;
                 padding: 12px 20px 12px 50px;
                }
-               .team {
-                //display: flex;
-                // flex-direction: row;
-                //align-items: center;
-                //justify-content: center;
-                //background-color: Beige   ;
-                height: 600px;
-                //   padding:  0  80px; 
-                //box-shadow: rgba(0,0,0,0.2) 0px 1px 2px;
-                //width:100%;
-                //height:100%;
-                position:center;
-                background:url('/static/images/bg3.png'), no-repeat;
-                background-size: cover;
-                }
-                .search {
-                    display: flex;
-                    justify-content: center;
-                    width: 100%;
-                    height: 50px;
-                    background-color:white;
-                }
-                .box1 {
-                    width: 120px;
-                    height: 50px;
-                    line-height: 50px; 
-                    background: white;
-                    color: white;
-                    background-color: Aqua ;
-                    border: 1px solid #CCD1D9;
-                    text-align: center; 
-                    margin-left: 35px;
-                    font-size: 25px;
-                    border-radius: 25px;
-                  }
-                  .text-search {
-                    border: 2px solid white;
-                    box-shadow: rgba(0,0,0,0.3) 0px 1px 2px;
-                    width: 500px;
-                  }
-                  .icon-search {
-                    padding: 10px;
-                    background: dodgerblue;
-                    color: white;
-                    min-width: 35px;
-                    text-align: center;
-                    background:url('/static/images/search-515.png') no-repeat;
-                  }
-                  .icon-stock {
+                .icon-stock {
                     background:url('/static/images/stock.png') no-repeat;   
                   }
-                  .introduct {
-                      display: flex;
-                      justify-content: center;
-                      margin-top: 50px;
-                      height:200px;
-                  }
-                  .boxL-introduct {
-                        width:300px;
-                        height:170px;
-                        background:white;
-                        color:black;
-                        border-right: 1px solid black;
-                        text-align: center;
-                  }
-                  .boxR-introduct {
-                    width:300px;
-                    height:170px;
-                    background:white;
-                    color:black;
-                    border-left: 1px solid black;
-                    text-align: center;
-                }
-                  
                   h3 {
                     opacity: 0.7;
                 }
-                input[type=text] {
-                    width: 500px;
-                    box-sizing: border-box;
-                    border: 2px solid #ccc;
-                    border-radius: 4px;
-                    font-size: 16px;
-                    background-color: white;
-                    background-image: url('/static/images/search-icon-hi.png');
-                    background-position: 10px 10px; 
-                    background-repeat: no-repeat;
-                    padding: 12px 20px 12px 50px;
-                    -webkit-transition: width 0.4s ease-in-out;
-                    transition: width 0.4s ease-in-out;
-                  }
-                
-                
            `}</style>
-
-        </div> // JSX-HTML
+        </div>
     )
 }
 export default MainLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,176 @@
 import MainLayout from '../layouts/MainLayout';
+import React, { useState } from 'react';
 
 const HomePage = () => {
+
+    const [behind] = useState('../static/images/background-index.jpg')
     return (
-        <MainLayout>
-            <div className="main">
-                
+        <div>
+            <MainLayout>
+            </MainLayout>
+
+            <div className='flexbg'>
+                <div className="layoutBackground">
+                    <img src={behind} className="behindground"></img>
+                </div>
             </div>
-            <style JSX>{`
-                .main {
-                    display: flex;
-                    background:red;
-                    //hight:100px;
-                    //width: 70%;
-                    //padding:  0  80px; 
-                    justify-content: center;
+            <div className='search'>
+                <input className='text-search' type="text" placeholder="Search.." >
+                </input>
+                <button className="box1" type="submit">Search</button>
+            </div>
+            <div className="introduct">
+                <div className="boxL-introduct">
+                    <h1>Storage</h1>
+                    <h3>Keep your product data <br></br> into the database</h3>
+                </div>
+                <div className="boxR-introduct">
+                    <h1>Accessible</h1>
+                    <h3>The product Information <br></br> can be easily <br></br> accessed by anynoe</h3>
+                </div>
+            </div>
+            <div className='flexHOT' >
+                <div className='Box_hotpro'>
+                    <span className="orangeText">Hot</span> <span className="blackText" Product>Product</span>
+                </div>
+            </div>
+
+            <style jsx global>{`
+                @font-face{
+                    font-family: Roboto;
+                    src: url(/static/fonts/Roboto-Regular.ttf);
+                    font-weight: 400;
                 }
-                .box1 {
-                    width: 70%;
-                    //height: 500px;
-                    line-height: 50px; 
-                    background:red;
-                    color: OrangeRed ;
-                    text-align: flex-start; 
-                    padding-left: 30px;
-                    border-top: 1px solid black;
+                body {
+                    margin: 0; 
+                    font-family: 'Roboto'
+                }
+           `}</style>
+            <style jsx>{`
+            .flexbg{
+               // margin-top:3px;
+            }
+            .layoutBackground{
+                display: flex;
+                flex-direction:column;
+                justify-content: flex-end;
+                align-items:center;
+                width:100%;
+            }
+            .search {
+                    display: flex;
+                    justify-content: center;
+                    width: 100%;
+                    height: 50px;
+                    background-color:white;
+                }
+            .behindground{
+                background-size:auto;
+                width:100%;
+            }
+            input[type=text] {
+                    width: 500px;
+                    box-sizing: border-box;
+                    border: 2px solid #ccc;
+                    border-radius: 4px;
+                    font-size: 16px;
+                    background-color: white;
+                    background-image: url('/static/images/search-icon-hi.png');
+                    background-position: 10px 10px; 
+                    background-repeat: no-repeat;
+                    padding: 12px 20px 12px 50px;
+                    -webkit-transition: width 0.4s ease-in-out;
+                    transition: width 0.4s ease-in-out;
+                    border-radius: 25px;
                   }
+            .introduct {
+                    display: flex;
+                    justify-content: center;
+                    margin-top: 50px;
+                    height:200px;
+                    //background: gray;
+                }
+            .boxL-introduct {
+                      width:300px;
+                      height:150px;
+                      background:white;
+                      color:black;
+                      border-right: 1px solid black;
+                      text-align: center;
+                      
+                }
+            .boxR-introduct {
+                  width:300px;
+                  height:150px;
+                  background:white;
+                  color:black;
+                 // border-left: 1px solid black;
+                  text-align: center;
+              }
+            h3 {
+                  opacity: 0.7;
+              }
+            .search {
+                display: flex;
+                justify-content: center;
+                width: 100%;
+                height: 50px;
+                margin-top:10px;
+                background-color:white;
+            }
+            .box1 {
+                width: 120px;
+                height: 50px;
+                line-height: 50px; 
+                background: white;
+                color: white;
+                background-color: Aqua ;
+                border: 1px solid #CCD1D9;
+                text-align: center; 
+                margin-left: 35px;
+                font-size: 25px;
+                border-radius: 25px;
+              }
+              .text-search {
+                border: 2px solid white;
+                box-shadow: rgba(0,0,0,0.3) 0px 1px 2px;
+                width: 500px;
+    
+              }
+              .icon-search {
+                padding: 10px;
+                background: dodgerblue;
+                color: white;
+                min-width: 35px;
+                text-align: center;
+                background:url('/static/images/search-515.png') no-repeat;
+              }
+              .flexHOT{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+              }
+              .Box_hotpro{
+                width: 70%;
+                    line-height: 60px; 
+                    color: black;
+                    font-size: 20px;
+                    border-top: 1px solid black;
+                    text-indent: 20px;
+                    align-self: flex-start;
+              }
+              .orangeText{
+                font-size:25px;
+                color:orange;
+                font-weight:bold;
+              }
+              .blackText{
+                font-size:25px;
+                color:black;
+                font-weight:bold;
+              }
             `}</style>
-        </MainLayout>
+        </div>
 
     )
 }
